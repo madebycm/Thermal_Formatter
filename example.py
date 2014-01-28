b2b22c024c4e757691bd729cdd916bdb7d5709e5
madebycm <christian.meinhold@gmail.com>
2014-01-28 08:46:15 +0100

    Modularize into class Thermal_Formatter

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
-from Thermal_Formatter import formatter 
+from Thermal_Formatter import Thermal_Formatter as formatter
+formatter = formatter.Thermal_Formatter()
 
 data = [
     ['s',   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."]
