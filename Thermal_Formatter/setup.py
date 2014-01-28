2d4b1bfd7905e078592d851b5568957082bae4bc
madebycm <christian.meinhold@gmail.com>
2014-01-28 19:38:43 +0100

    Create package
    pip install Thermal_Formatter-0.1.tar.gz

---
+++ Thermal_Formatter/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='Thermal_Formatter',
+      version='0.1',
+      author='madebycm',
+      author_email='christian.meinhold@gmail.com',
+      license='MIT',
+      description='A python module to prepare output for printing w/ a thermal printer',
+      py_modules=['Thermal_Formatter'])
